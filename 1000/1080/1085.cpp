ab330b83901430d7033b107787b44143d3b0141d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-02-13 23:43:21 +0900

    설명 추가

--- 1000/1080/1085.cpp
+++ 1000/1080/1085.cpp
@@ -9,7 +9,13 @@
  
 #include <stdio.h>
 int main(){
-    int n;scanf("%d",&n);int sum=0;
-    for(int i=0;i<n;i++){int t;scanf("%d",&t);sum+=t;}
-    printf("%.2lf",(double)sum/n);
+    int n;scanf("%d",&n);
+    int sum=0; // 총합을 저장하는 변수 sum
+    int t; 
+    for(int i=0;i<n;i++){ // n번 반복 
+        scanf("%d",&t); 
+        // t에 값을 다시 입력받아!
+        sum+=t; // t의 값을 sum에 저장해 주자~!
+    }
+    printf("%.2lf",(double)sum/n); // 형식에 맞게 출력
 }
