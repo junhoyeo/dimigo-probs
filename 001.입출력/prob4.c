1cba53447e4d76d3cd2b41f2d726ee2e00eac938
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-03-18 12:48:43 +0900

    update: print char with shift algo

--- 001.입출력/prob4.c
+++ 001.입출력/prob4.c
@@ -4,3 +4,9 @@ int main() {
 	scanf("%c", &a);
 	printf("%c", a-32);
 }
+
+// 하나씩 shift 해서 출력하기
+
+// n = input()
+// print (chr((ord(n) + 1 - ord('A')) % 26 + ord('A')))
+// # 따라서 (n+1-'A')%26 + 'A'
