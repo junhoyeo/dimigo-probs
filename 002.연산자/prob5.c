f31cb67142007af28e2a7c20a5803f55f9d1e92c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-03-19 14:38:09 +0900

    update: 연산자 문제 5, 6 설명 추가

--- 002.연산자/prob5.c
+++ 002.연산자/prob5.c
@@ -2,4 +2,7 @@
 int main() {
 	int a; scanf("0x%X", &a);
 	printf("0x%04hx", ~a);
+
+	// https://stackoverflow.com/questions/8441257/why-does-c-print-my-hex-values-incorrectly#answer-8441401
+	// integer type-promotion 때문에 length modifier를 붙여줘야 함
 }
--- 002.연산자/prob6.c
+++ 002.연산자/prob6.c
@@ -2,4 +2,5 @@
 int main() {
 	int a; scanf("0x%X", &a);
 	printf("0x%04hx", a | 0x5555);
+	// 010101 ... 패턴의 0x5555와 비트 OR 연산을 함
 }
